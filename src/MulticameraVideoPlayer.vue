9218160e91a53ac4a95a2c18798ae3eda3f54d14
Matej Čupka <matocupka@me.com>
2020-01-15 15:29:57 +0100

    Simple component

---
+++ src/MulticameraVideoPlayer.vue
@@ -0,0 +1,21 @@
+<template>
+    <p>{{greeting}} World!</p>
+</template>
+
+<script>
+    export default {
+        name: 'MulticameraVideoPlayer',
+        data() {
+            return {
+                greeting: 'Hello'
+            };
+        },
+        mounted() {
+            // eslint-disable-next-line no-console
+            console.log('Mounted');
+        }
+    }
+</script>
+
+<style scoped>
+</style>
---
+++ src/entry.js
@@ -0,0 +1,28 @@
+import component from './MulticameraVideoPlayer.vue';
+
+function install(Vue) {
+    if (install.installed) {
+        return;
+    }
+    install.installed = true;
+    Vue.component('MulticameraVideoPlayer', component)
+}
+
+const plugin = {
+    install
+};
+
+let GlobalVue = null;
+if (typeof window !== 'undefined') {
+    GlobalVue = window.Vue;
+} else if (typeof global !== 'undefined') {
+    GlobalVue = global.vue;
+}
+
+if (GlobalVue) {
+    GlobalVue.use(plugin);
+}
+
+component.install = install;
+
+export default component;
