811d9eef731657196f4889de899f0ed4093a76d0
Matej Čupka <matocupka@me.com>
2020-03-04 20:05:55 +0100

    Fullscreen

--- src/MulticameraVideoPlayer.vue
+++ src/MulticameraVideoPlayer.vue
@@ -1,13 +1,15 @@
 <template>
   <div class="playerWrapper">
-    <div class="playerContainer">
+    <div class="playerContainer"
+         :id="`playerContainer-${id}`"
+         @dblclick="onDblclick($event)">
       <div class="playerElement"
            :class="{active: cameraKey === cameraActive}"
            :style="getVideoPosition(cameraKey)"
+           @click="onClick($event, cameraKey)"
            :key="cameraKey"
            v-for="cameraKey in cameraKeys">
-        <video :ref="cameraKey"
-               @click="onClick($event, cameraKey)"/>
+        <video :ref="cameraKey"/>
       </div>
     </div>
   </div>
@@ -18,6 +20,50 @@
     import * as HLS from 'hls.js';
     import Plyr from 'plyr';
     import {v4 as uuid} from 'uuid';
+
+    window.multiCameraVideoPlayer = {
+        onFullscreenToggle(e, id) {
+            e.preventDefault();
+            e.stopPropagation();
+            e.stopImmediatePropagation();
+
+            if (document.fullscreenElement || document.webkitFullscreenElement || document.mozFullScreenElement || document.msFullscreenElement) {
+                window.multiCameraVideoPlayer.fullscreenExit();
+            } else {
+                window.multiCameraVideoPlayer.fullscreenEnter(id);
+            }
+        },
+        fullscreenEnter(id) {
+            const playerSelector = `div#playerContainer-${id}`;
+            const playerContainer = document.querySelector(playerSelector);
+            const video = document.querySelector(`${playerSelector} video`);
+
+            if (playerContainer.requestFullscreen) {
+                playerContainer.requestFullscreen();
+            } else if (playerContainer.msRequestFullscreen) {
+                playerContainer.msRequestFullscreen();
+            } else if (playerContainer.mozRequestFullScreen) {
+                playerContainer.mozRequestFullScreen();
+            } else if (playerContainer.webkitRequestFullscreen) {
+                playerContainer.webkitRequestFullscreen();
+            } else if (video.webkitEnterFullScreen) {
+                video.webkitEnterFullScreen();
+            } else {
+                throw new Error(`Full screen isn't supported`);
+            }
+        },
+        fullscreenExit() {
+            if (document.exitFullscreen) {
+                document.exitFullscreen();
+            } else if (document.msExitFullscreen) {
+                document.msExitFullscreen();
+            } else if (document.mozCancelFullScreen) {
+                document.mozCancelFullScreen();
+            } else if (document.webkitExitFullscreen) {
+                document.webkitExitFullscreen();
+            }
+        },
+    };
 
     export default {
         name: 'MulticameraVideoPlayer',
@@ -38,12 +84,6 @@
                 default: false,
             },
 
-            controls: {
-                type: Array,
-                required: false,
-                default: () => ['play-large', 'play', 'mute', 'volume', 'captions', 'settings', 'fullscreen'],
-            },
-
             cameras: {
                 type: Object,
                 required: true,
@@ -51,17 +91,40 @@
         },
         data() {
             return {
-                id: `player-${uuid()}`,
-
+                id: uuid(),
                 cameraKeys: Object.keys(this.cameras),
                 cameraActive: Object.keys(this.cameras)[0],
-                players: {},
-
-                controls_settings: ['captions', 'quality']
+                players: {}
             };
         },
         computed: {},
         methods: {
+            getControls() {
+                return `<div class="plyr__controls">
+                    <button type="button" class="plyr__control" aria-label="Play, {title}" data-plyr="play" style="margin-right: auto;">
+                        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-pause"></use></svg>
+                        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-play"></use></svg>
+                        <span class="label--pressed plyr__tooltip" role="tooltip">Pause</span>
+                        <span class="label--not-pressed plyr__tooltip" role="tooltip">Play</span>
+                    </button>
+
+                    <button type="button" class="plyr__control" aria-label="Mute" data-plyr="mute">
+                        <svg class="icon--pressed" role="presentation"><use xlink:href="#plyr-muted"></use></svg>
+                        <svg class="icon--not-pressed" role="presentation"><use xlink:href="#plyr-volume"></use></svg>
+                        <span class="label--pressed plyr__tooltip" role="tooltip">Unmute</span>
+                        <span class="label--not-pressed plyr__tooltip" role="tooltip">Mute</span>
+                    </button>
+                    <div class="plyr__volume">
+                        <input data-plyr="volume" type="range" min="0" max="1" step="0.05" value="1" autocomplete="off" aria-label="Volume">
+                    </div>
+                    <button type="button" class="plyr__control plyr__control" onclick="multiCameraVideoPlayer.onFullscreenToggle(event, '${this.id}')">
+                        <svg class="fullscreen-show" role="presentation"><use xlink:href="#plyr-exit-fullscreen"></use></svg>
+                        <svg class="fullscreen-hide" role="presentation"><use xlink:href="#plyr-enter-fullscreen"></use></svg>
+                        <span class="fullscreen-show plyr__tooltip" role="tooltip">Exit fullscreen</span>
+                        <span class="fullscreen-hide plyr__tooltip" role="tooltip">Enter fullscreen</span>
+                    </button>
+                </div>`;
+            },
             getVideoPosition(cameraKey) {
                 if (this.cameraActive === cameraKey) {
                     return {};
@@ -74,7 +137,9 @@
                 const [x, y] = this.cameras[this.cameraActive].cameraPositions[cameraKey];
                 return {top: `${-50 + y}%`, left: `${-50 + x}%`};
             },
-
+            onDblclick(e) {
+                window.multiCameraVideoPlayer.onFullscreenToggle(e, this.id);
+            },
             onClick(e, cameraKey) {
                 if (cameraKey === this.cameraActive) {
                     return;
@@ -85,8 +150,6 @@
                 e.stopImmediatePropagation();
 
                 console.log(cameraKey);
-
-                // TODO: Take in mind fullscreen
             },
 
             onLoad() {
@@ -152,10 +215,11 @@
                 const video = this.$refs[cameraKey];
 
                 const player = new Plyr(video, {
-                    controls: this.controls,
+                    controls: this.getControls(),
                     settings: this.controls_settings,
                     autoplay: false,
                     muted: true,
+                    fullscreen: {enabled: false}
                 });
 
                 player.on('ready', e => {
@@ -170,26 +234,25 @@
                     });
                 });
 
-                player.on('playing', () => {
-                    this.onPlay(cameraKey, player);
+                player.on('playing', (e) => {
+                    this.onPlay(cameraKey, player, e);
                 });
-                player.on('play', () => {
-                    this.onPlay(cameraKey, player);
+                player.on('play', (e) => {
+                    this.onPlay(cameraKey, player, e);
                 });
-                player.on('pause', () => {
-                    this.onPause(cameraKey, player);
+                player.on('pause', (e) => {
+                    this.onPause(cameraKey, player, e);
                 });
 
-                player.on('volumechange', () => {
-                    this.onVolumechange(cameraKey, player);
+                player.on('volumechange', (e) => {
+                    this.onVolumechange(cameraKey, player, e);
                 });
             });
         }
     }
 </script>
 
-<style scoped
-       lang="scss">
+<style lang="scss">
   @import 'https://cdn.plyr.io/3.5.10/plyr.css';
 
   div.playerWrapper {
@@ -200,6 +263,39 @@
       height: 0;
       padding-bottom: calc(9 / 16 * 100%);
 
+      &:not(:hover) {
+        div.playerElement:not(.active) {
+          display: none;
+        }
+      }
+
+      &:fullscreen {
+        height: 100%;
+        padding-bottom: 0;
+
+        .fullscreen- {
+          &show {
+            display: block;
+          }
+
+          &hide {
+            display: none;
+          }
+        }
+      }
+
+      &:not(:fullscreen) {
+        .fullscreen- {
+          &hide {
+            display: block;
+          }
+
+          &show {
+            display: none;
+          }
+        }
+      }
+
       div.playerElement {
         position: absolute;
         width: 100%;
@@ -208,10 +304,6 @@
 
         &:not(.active) {
           z-index: 15;
-          -webkit-box-shadow: 0 0 2px 0 rgba(255, 255, 255, 0.5);
-          -moz-box-shadow: 0 0 2px 0 rgba(255, 255, 255, 0.5);
-          box-shadow: 0 0 2px 0 rgba(255, 255, 255, 0.5);
-
           transition: 0.1s;
           transform: scale(0.1);
           transform-origin: center;
@@ -219,12 +311,6 @@
           &:hover {
             transform: scale(0.2);
           }
-        }
-      }
-
-      &:not(:hover) {
-        div.playerElement:not(.active) {
-          display: none;
         }
       }
     }
